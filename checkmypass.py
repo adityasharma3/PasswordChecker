24d7e0be2258fbe0ba3eed47b30a0558aa8a8ca8
Aditya Sharma <aditya.sharma9301@gmail.com>
2020-07-11 23:57:36 +0530

    intially initial

--- checkmypass.py
+++ checkmypass.py
@@ -1,7 +1,19 @@
 import requests 
+import hashlib
 
 url = 'https://api.pwnedpasswords.com/range/' + 'password123'
 
 res = requests.get(url)
-
 print(res)
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching : {res.status_code} , check the api and try again.')
+    return res
+
+def pwned_api_check(password):
+    pass
+
+request_api_data('123')
