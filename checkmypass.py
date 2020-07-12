b886f7e2da307eb6c726648a3074542af5118952
Aditya Sharma <53974211+adityasharma3@users.noreply.github.com>
2020-07-12 23:36:40 +0530

    final commit

--- checkmypass.py
+++ checkmypass.py
@@ -1,10 +1,10 @@
 import requests 
 import hashlib
+import sys
 
 url = 'https://api.pwnedpasswords.com/range/' + 'password123'
 
 res = requests.get(url)
-print(res)
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -13,7 +13,29 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching : {res.status_code} , check the api and try again.')
     return res
 
-def pwned_api_check(password):
-    pass
+def get_password_leaks_count(hashes , hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h , count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
-request_api_data('123')
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char , tail = sha1password[:5] , sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char , tail)
+    return get_password_leaks_count(response , tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times, you should probably change your password.')
+        else:
+            print(f'{password} was not found.')
+
+    return "done"
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
